705d4212544e0c48c405bf31806daa2728f80177
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-01 02:13:00 +0000

    Fetch data from TheCatAPI

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,5 @@
 const request = require('request');
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+  const data = JSON.parse(body);
+  console.log(data[0]);
+});
