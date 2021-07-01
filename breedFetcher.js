07f122437a1b5a47bf3e4b3825d551bcc6e5afda
Kathy <kathy.tam@mail.mcgill.ca>
2021-07-01 02:23:48 +0000

    Handle edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,16 @@
+const breed = process.argv.slice(2);
+
 const request = require('request');
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
   const data = JSON.parse(body);
-  console.log(data[0]);
+  if (!data.length) {
+    console.log(`Requested breed not found.`);
+    return;
+  }
+  console.log(data[0].description);
 });
